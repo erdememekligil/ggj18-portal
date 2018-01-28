5bd3ecffafa01258243896ce7fb2191f95190279
Burak Goncu <bmgoncu@Buraks-MacBook-Pro.local>
2018-01-28 12:08:28 +0300

    Ui components added

---
+++ Assets/Scripts/ControlPanelUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPanelUI : MonoBehaviour {
+    public GaugeUI Gauge;
+    public Slider PowerSlide;
+    public Toggle ChannelButton1, ChannelButton2, ChannelButton3, ChannelButton4;
+    public Image ChannelScreen1, ChannelScreen2, ChannelScreen3, ChannelScreen4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GaugeUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeUI : MonoBehaviour {
+    public RectTransform Handle;
+    public float MinAngle;
+    public float MaxAngle;
+    [Range(0,1)]
+    public float Value;
+	
+	// Update is called once per frame
+	void Update () {
+        Handle.localRotation = Quaternion.Euler(0, 0, Value * (MaxAngle - MinAngle) + MinAngle);
+	}
+}
